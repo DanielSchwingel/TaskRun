94a1b68a9e99faa8d0414c80ad703c5645c670c8
unknown <danielfilipeschwingel@gmail.com>
2020-07-19 01:10:49 -0300

    Adjusted style sidebar

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { View, TouchableOpacity } from 'react-native';
+import { View, Image, TouchableOpacity, Text, ImageBackground } from 'react-native';
 import { DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 
 import styles from './styles';
+import logoImg from '../../assets/images/logo.png';
 
 export default function SideBar(props){
     
@@ -15,14 +16,36 @@ export default function SideBar(props){
         <DrawerContentScrollView style={styles.container}>
             <View style={styles.header}>
                 <View style={styles.userInfo}>
-
+                    <Image source={logoImg} style={styles.imageLogo} />
+                    <View>
+                        <Text style={styles.title}>TaskRun</Text>
+                        <Text style={[styles.subtitle, {paddingLeft: 15}]}>Menu de navegação</Text>
+                    </View>
                 </View>
                 <TouchableOpacity onPress={closeDrawer}>
-                    <Icon name={'times'} size={28} color='#5B2121' />
+                    <Icon name={'times'} size={20} color='#5B2121' />
                 </TouchableOpacity>
             </View>
-            
-            <DrawerItemList {...props}  />
+            <View style={styles.body}>
+                <DrawerItemList
+                    itemStyle= {
+                        {
+                            marginHorizontal: 0, 
+                            marginVertical: 0
+                        }
+                    }
+                    labelStyle={styles.title}
+                    activeTintColor='red'
+                {...props}  
+                />
+            </View>            
+            <View style={styles.footer}>
+                <TouchableOpacity omPress={()=>{}} style={styles.logout}>
+                    <Icon name={'power-off'} size={20} color='#5B2121' />
+                    <Text style={styles.title}>Logout</Text>
+                </TouchableOpacity>
+                <Text style={[styles.subtitle,{marginTop: 70}]}> Versão 1.0.0</Text>
+            </View>
         </DrawerContentScrollView>
     )
 
--- src/components/SideBar/styles.js
+++ src/components/SideBar/styles.js
@@ -9,13 +9,49 @@ export default StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
-        paddingRight: 15
+        paddingRight: 15,
+        marginBottom: 20
     },
 
-    userInfo : {
+    userInfo: {
         backgroundColor: '#FFF',
+        alignItems: 'center',
+        justifyContent: 'center',
         height: 90,
         width: '70%',
-        borderBottomEndRadius: 20
+        borderBottomEndRadius: 20,
+        flexDirection: 'row'
+    },
+    
+    imageLogo: {
+        width: 50,
+        height: 50
+    },
+
+    body: {
+        // backgroundColor: 'blue'       
+    },
+
+    footer: {
+        marginTop: 350,
+        padding: 15
+    },
+
+    logout: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+
+    title: {
+        fontFamily: 'Ubuntu-Bold',
+        fontSize: 16,
+        color: '#5B2121',
+        marginLeft: 15,
+    },
+
+    subtitle: {
+        fontFamily: 'Ubuntu-Regular',
+        fontSize: 12,
+        color: '#9E7D7B'
     }
 })
